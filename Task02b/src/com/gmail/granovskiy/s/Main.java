c991ede2868fe16b3191e8355415f4ca5c3d7853
SviatoslavExpert <s.granovskiy@gmail.com>
2018-10-24 13:53:18 +0300

    Second commit

--- Task02b/src/com/gmail/granovskiy/s/Main.java
+++ Task02b/src/com/gmail/granovskiy/s/Main.java
@@ -1,10 +1,23 @@
+/*
+Java Start Task 02.02
+Написать программу которая вычислит и выведет на экран
+площадь треугольника если известны его стороны.
+*/
+
 package com.gmail.granovskiy.s;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main (String []args){
+    	
+        double a = 0.3;
+        double b = 0.4;
+        double c = 0.5;
+        
+        double p = (a + b + c)/2.0;
+        double area = Math.sqrt(p*(p - a)*(p - b)*(p - c));
+        System.out.println("Triangle area: " + area);
+        
+    }
+    
 }
--- Task02c/src/com/gmail/granovskiy/s/Main.java
+++ Task02c/src/com/gmail/granovskiy/s/Main.java
@@ -1,10 +1,26 @@
+/*
+Java Start Task 02.03
+
+Написать программу которая вычислит и выведет на экран
+длину окружности, если ее радиус считывается с клавиатуры.
+*/
+
 package com.gmail.granovskiy.s;
+
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-            System.out.println("How are you?");
-	}
+    public static void main (String []args){
+    	
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input radius value:");
+        
+        double radius = sc.nextDouble();
+        double perimeter = 2.0 * Math.PI * radius;
+        System.out.println("Perimeter = " + perimeter);
+        sc.close();
+        
+    }
 
 }
