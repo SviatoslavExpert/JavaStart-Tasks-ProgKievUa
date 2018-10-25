caf7d2a2e450ef0b3f70d0d78f786935c9e1dd2f
SviatoslavExpert <s.granovskiy@gmail.com>
2018-10-25 09:12:45 +0300

    Initial commit

---
+++ Task03a/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,43 @@
+/*
+Java Start Task 03.01
+Написать программу которая считывает 4 числа и выводит на экран
+самое большое из них.
+ */
+
+package com.gmail.granovskiy.s;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		int b;
+		int c;
+		int d;
+		int max;
+
+		System.out.println("Input a:");
+		a = sc.nextInt();
+		System.out.println("Input b:");
+		b = sc.nextInt();
+		System.out.println("Input c:");
+		c = sc.nextInt();
+		System.out.println("Input d:");
+		d = sc.nextInt();
+
+		max = select(select(a, b), select(c, d));
+		System.out.println("The maximum number is: " + max);
+		sc.close();
+
+	}
+
+	public static int select(int x, int y) {
+		if (x > y)
+			return x;
+		else
+			return y;
+	}
+
+}
