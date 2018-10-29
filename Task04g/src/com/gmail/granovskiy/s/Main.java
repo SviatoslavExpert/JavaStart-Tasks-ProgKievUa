109de9806a6291584f42cebddd3ffe6fa85f6b9b
SviatoslavExpert <s.granovskiy@gmail.com>
2018-10-29 19:24:02 +0200

    Initial commit

--- Task04g/src/com/gmail/granovskiy/s/Main.java
+++ Task04g/src/com/gmail/granovskiy/s/Main.java
@@ -14,41 +14,41 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter width (odd number):");
-        int width = sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter width (odd number):");
+		int width = sc.nextInt();
 
-        if(width%2 != 0){
+		if (width % 2 != 0) {
 
-            for (int i = 1; i <= width/2 + 1; i++) {
-                for (int j = 1; j <= width; j++) {
+			for (int i = 1; i <= width / 2 + 1; i++) {
+				for (int j = 1; j <= width; j++) {
 
-                    if (j >= i && j <= width - i + 1) {
-                        System.out.print("*");
-                    } else {
-                        System.out.print(" ");
-                    }
-                }
-                System.out.println();
-            }
+					if (j >= i && j <= width - i + 1) {
+						System.out.print("*");
+					} else {
+						System.out.print(" ");
+					}
+				}
+				System.out.println();
+			}
 
-            for (int i = width/2; i > 0; i--) {
-                for (int j = 1; j <= width; j++) {
+			for (int i = width / 2; i > 0; i--) {
+				for (int j = 1; j <= width; j++) {
 
-                    if (j >= i && j <= width - i + 1) {
-                        System.out.print("*");
-                    } else {
-                        System.out.print(" ");
-                    }
-                }
-                System.out.println();
-            }
-        }else {
-            System.out.print("You entered even number.");
-        }
-        sc.close();
-    }
+					if (j >= i && j <= width - i + 1) {
+						System.out.print("*");
+					} else {
+						System.out.print(" ");
+					}
+				}
+				System.out.println();
+			}
+		} else {
+			System.out.print("You entered even number.");
+		}
+		sc.close();
+	}
 
 }
