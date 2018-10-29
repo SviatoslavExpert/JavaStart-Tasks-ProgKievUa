7f2730294dd0ebab32e49d9b327a5f5d7a666ea9
SviatoslavExpert <s.granovskiy@gmail.com>
2018-10-29 07:27:25 +0200

    Initial commit

--- Task04a/src/com/gmail/granovskiy/s/Main.java
+++ Task04a/src/com/gmail/granovskiy/s/Main.java
@@ -10,11 +10,31 @@
 
 package com.gmail.granovskiy.s;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int columns;
+		int rows;
 
+		System.out.println("Enter the amount of columns:");
+		columns = sc.nextInt();
+		System.out.println("Enter the length of rows:");
+		rows = sc.nextInt();
+
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				if (j % 2 == 0) {
+					System.out.print("***");
+				} else {
+					System.out.print("+++");
+				}
+			}
+			System.out.println();
+		}
+		sc.close();
 	}
 
 }
