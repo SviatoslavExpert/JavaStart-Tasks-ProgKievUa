888ab7cc8f9c1f8f0c6dbd2bbf9cf48188aa333a
SviatoslavExpert <s.granovskiy@gmail.com>
2018-10-30 18:12:08 +0200

    Initial commit

---
+++ Task07a/src/com/gmail/granovskiy/s/Main.java
@@ -0,0 +1,34 @@
+/*
+Написать программу которая вернет количество миллисекунд
+прошедших от такого же числа, но в прошлом месяце до сегодняшней
+даты. Например если сегодня 3 августа, то узнать сколько миллисекунд
+прошло с 3 июля.
+ */
+
+package com.gmail.granovskiy.s;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calendar calOne = Calendar.getInstance();
+		Calendar calTwo = Calendar.getInstance();
+
+		calTwo.set(Calendar.MONTH, calOne.get(Calendar.MONTH) - 1);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yy");
+
+		Date dateOne = calOne.getTime();
+		Date dateTwo = calTwo.getTime();
+
+		long t = dateOne.getTime() - dateTwo.getTime();
+
+		System.out.println("From " + sdf.format(calOne.getTime()) + " to " + sdf.format(calTwo.getTime()) + ":");
+		System.out.println(t + " milliseconds");
+	}
+
+}
